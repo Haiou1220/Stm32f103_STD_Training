40001c0843549067a445ee582da1234f697a937f
Haiou1220 <guohaihang@outlook.com>
2024-06-05 18:14:12 +0800

    std_ssd1306_demo

---
+++ 240601_STD_ssd1306_demo/USER/MAIN.C
@@ -0,0 +1,128 @@
+/**
+  ******************************************************************************
+  * @file    GPIO/IOToggle/main.c 
+  * @author  MCD Application Team
+  * @version V3.6.0
+  * @date    20-September-2021
+  * @brief   Main program body.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2011 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "stdio.h"
+#include "ssd1306.h"
+
+/*Compiler definec : USE_STDPERIPH_DRIVER,STM32F10X_MD */
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+#define Led0_On GPIO_WriteBit( GPIOB,GPIO_Pin_5, Bit_RESET );
+#define Led0_Off GPIO_WriteBit( GPIOB,GPIO_Pin_5, Bit_SET );
+#define Led1_On GPIO_WriteBit( GPIOE,GPIO_Pin_5, Bit_RESET );
+#define Led1_Off GPIO_WriteBit( GPIOE,GPIO_Pin_5, Bit_SET );
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+void led_init(void )//led0 led1 pb5 pe5
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOE, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOB,&GPIO_InitStruct);//pb5
+  GPIO_Init(GPIOE,&GPIO_InitStruct);//pb5
+
+}
+
+void usart1_init(uint32_t BaudRate) //pa9 tx,  pa10 rx 中断接收
+{
+
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA|RCC_APB2Periph_USART1,   ENABLE);//开启外设时钟
+	//配置gpio
+	GPIO_InitTypeDef  GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init( GPIOA, & GPIO_InitStruct);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init( GPIOA, & GPIO_InitStruct);
+	//配置usart1
+	USART_InitTypeDef  USART_InitStruct;
+	USART_InitStruct.USART_BaudRate = BaudRate;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_Mode = USART_Mode_Rx|USART_Mode_Tx;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_Init(  USART1, &USART_InitStruct);
+	//配置串口中断
+	NVIC_InitTypeDef  NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 2;
+	NVIC_Init(& NVIC_InitStruct);
+	//开启串口中断
+	USART_ITConfig( USART1,   USART_IT_RXNE,  ENABLE );
+	//开启串口
+	USART_Cmd( USART1, ENABLE );
+
+}
+
+int fputc(int c,FILE* f)
+{
+																																				  
+	while ( USART_GetFlagStatus(   USART1,  USART_FLAG_TXE ) != SET) {}; //等待TXE=1发送寄存器空，表示发送完成，由硬件复位，TC与TXE复位值为1
+	USART_SendData(  USART1,  c);
+	
+	return c;
+}
+int main(void)
+{
+ NVIC_PriorityGroupConfig(  NVIC_PriorityGroup_2);//配置中断分组
+ led_init();//led0 led1 pb5 pe5
+ usart1_init(115200 ) ;//pa9 tx,  pa10 rx
+  
+ OLED_bsp_init();
+ OLED_init();
+	
+  Led1_On;	
+  Led0_On;
+
+  {
+	  int len = 0;
+	  OLED_Clear();
+	  // OLED_ShowChar(0,0, 'L');
+	  len = OLED_ShowString(0, 0, "GHH_val:");
+	  OLED_PrintSignedVal(len, 0, -549);
+	  // OLED_ClearLine(0, 0);
+  }
+
+	printf("IN MAIN\r\n");
+	
+  while (1) {
+ 
+	}
+   
+   
+}
+
+
+
+
+
+
